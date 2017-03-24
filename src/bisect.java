2f33ce1a99e1bb30e0bf4d50d823cf18d044e914
ligia <rad_mult@yahoo.com>
2017-03-24 15:36:40 +0200

    treinumere

---
+++ src/bisect.java
@@ -0,0 +1,23 @@
+/**
+ * Created by andreea on 24.03.2017.
+ */
+import acm. program.*;
+
+import java..Console;
+
+public class bisect extends Console Program
+{
+    public void run();
+}
+System.out.println( " Acest program calculeaza anul bisect. ");
+int year = read Int( " Introduceti anul; ");
+ if(( year % 4 ==0) && year%!100==0);
+        {
+        System.out.println(" Anul nu  este bisect "+year);
+
+        }
+        else if(() year % 4==0) $$( year%400==0 );
+        {
+            System.out.println (" Anul bisect este " + year);
+
+        }
