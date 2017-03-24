b1122c92a483808f61283739e533a8f80b6ce36d
ligia <rad_mult@yahoo.com>
2017-03-24 12:27:34 +0200

    hello

---
+++ src/Ex1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by andreea on 18.03.2017.
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        System.out.println("salut");
+    }
+}
---
+++ src/Ex2.java
@@ -0,0 +1,19 @@
+/**
+ * Created by andreea on 18.03.2017.
+ */
+/*public class Ex2 {
+
+    public static void main(String[] args) {
+        int nr = SkeletonJava.readIntConsole("numarul:"); // citire
+
+        if(nr>0) {` `
+
+        }
+
+        else
+        {
+
+        }
+        SkeletonJava.printConsole(nr); // afisare
+}
+}*/
