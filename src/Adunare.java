5a40579631a565d91dae95542fae8d0486ad5a56
ligia <rad_mult@yahoo.com>
2017-03-31 23:45:24 +0300

    factorial

---
+++ src/Adunare.java
@@ -0,0 +1,30 @@
+/**
+ * Created by andreea on 25.03.2017.
+ */
+/*
+public class Adunare {
+    public static void main(String[] args) {
+//se  citesc numere naturale pana  cand s e  introduce 0,
+        // afisati suma  obtinuta prin  adunarea  celor 2 numere
+        //
+        int x = 0;
+        int y = 0;
+        int suma;
+
+        do
+            {
+                 = SkeletonJava.readIntConsole(label:" ");
+                boolean conditie(>4)&&(<11);
+                if() {
+                    suma=   x+y;
+
+
+                }
+
+
+        }
+        while ( = !0);
+        System.out.println();
+    }
+}
+*/
---
+++ src/metode1.java
@@ -0,0 +1,22 @@
+/**
+ * Created by andreea on 25.03.2017.
+ */
+/*
+public class metode1 {
+    public static void main(String[] args) {
+        // sa  se citesca  un dtring  de  la  tastatura
+        // sa se afiseze stringul  in  upppercase
+
+        String s;
+        s="kfdjgljgro";//valori hardcodate
+        s= SkeletonJava.readStringConsole(la; " introduceti stringul");
+        System.out.println(s.toUpperCase());
+ String cale="c:\temp\tt\fisier,txt";
+ String extensie=cale.indexOf(".");
+String
+    }
+    }
+}
+
+
+*/
